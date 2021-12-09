2c9c577e34c438a8c31b544a88508d703a32bfae
Enguerrand Deladrière <84496053+LaGuezzz@users.noreply.github.com>
2021-12-09 23:43:33 +0100

    Update PlusOuMoins.js

--- PlusOuMoins/PlusOuMoins.js
+++ PlusOuMoins/PlusOuMoins.js
@@ -28,6 +28,7 @@ addtoarray("Dragon Blanc Aux Yeux Bleus",25,"Objects/DragonBlanc.png",objectarra
 addtoarray("Sèche-linge",499,"Objects/seche-linge.png",objectarray);
 addtoarray("Volant-Léopard",15,"Objects/volant-leopard.jpg",objectarray);
 addtoarray("Babouche-Aladin",139,"Objects/babouche-aladin.jpg",objectarray);
+addtoarray("Collection Intégrale Dora l'Exploratrice",99,"Objects/integral-dora.jpg",objectarray);
 
 function InputImage()
 {
