5f6ea4d4bd78ee6a263970aed4a7573406a18610
Enguerrand Deladrière <84496053+LaGuezzz@users.noreply.github.com>
2021-12-10 17:08:14 +0100

    Update PlusOuMoins.js

--- PlusOuMoins/PlusOuMoins.js
+++ PlusOuMoins/PlusOuMoins.js
@@ -29,6 +29,7 @@ addtoarray("Volant-Léopard",15,"Objects/volant-leopard.jpg",objectarray);
 addtoarray("Babouche-Aladin",139,"Objects/babouche-aladin.jpg",objectarray);
 addtoarray("Collection Intégrale Dora l'Exploratrice",95,"Objects/integral-dora.jpg",objectarray);
 addtoarray("Rat de compagnie",17,"Objects/rat-domestique.jpg",objectarray);
+addtoarray("Vinyle Aya Nakamura",16,"Objects/vinyle-nakamura.jpg",objectarray);
 
 function InputImage()
 {
