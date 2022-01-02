3c65c494d4dc80b4adbb790a10f3a0d23b685ad8
YohanJarnac <95044346+PapeYo@users.noreply.github.com>
2022-01-02 18:59:42 +0100

    Update shifumi_js.js

--- Shifumi/shifumi_js.js
+++ Shifumi/shifumi_js.js
@@ -1,38 +1,35 @@
 let userChoice;
 let final;
 
-function CISEAUX()
-{
+function CISEAUX() {
     document.getElementById("ordinateur").classList.remove("result")
     document.getElementById("resultat").classList.remove("result")
     userChoice = 0;
     console.log("Vous choisissez Ciseaux");
-    document.getElementById("Joueur").innerText="Ciseaux"
-    document.getElementById("ordinateur").innerText=""
-    document.getElementById("resultat").innerText=""
+    document.getElementById("Joueur").innerText = "Ciseaux"
+    document.getElementById("ordinateur").innerText = ""
+    document.getElementById("resultat").innerText = ""
 }
 
-function FEUILLE()
-{
+function FEUILLE() {
     document.getElementById("ordinateur").classList.remove("result")
     document.getElementById("resultat").classList.remove("result")
     userChoice = 1;
     console.log("Vous choisissez Feuille");
-    document.getElementById("Joueur").innerText="Feuille"
-    document.getElementById("ordinateur").innerText=""
-    document.getElementById("resultat").innerText=""
+    document.getElementById("Joueur").innerText = "Feuille"
+    document.getElementById("ordinateur").innerText = ""
+    document.getElementById("resultat").innerText = ""
 
 }
 
-function PIERRE()
-{
+function PIERRE() {
     document.getElementById("ordinateur").classList.remove("result")
     document.getElementById("resultat").classList.remove("result")
     userChoice = 2;
     console.log("Vous choisissez Pierre");
-    document.getElementById("Joueur").innerText="Pierre"
-    document.getElementById("ordinateur").innerText=""
-    document.getElementById("resultat").innerText=""
+    document.getElementById("Joueur").innerText = "Pierre"
+    document.getElementById("ordinateur").innerText = ""
+    document.getElementById("resultat").innerText = ""
 
 }
 
@@ -40,140 +37,121 @@ function PIERRE()
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
-  }
+}
 
-function JOUER()
-{
-    final = 0;
-    IA = getRandomInt(3);
+function JOUER() {
+    if (document.getElementById("PlayerName").value == "") {
+        alert("Veuillez indiquer votre pseudo ;-)");
+    } else {
+        final = 0;
+        IA = getRandomInt(3);
 
-    //Si l'utilisateur joue CISEAUX
-    if (userChoice == 0)
-    {
-       if (IA == 0)
-        {
-            document.getElementById("resultat").innerText="Match Nul"
-            final = 0; //Match nul
+        //Si l'utilisateur joue CISEAUX
+        if (userChoice == 0) {
+            if (IA == 0) {
+                document.getElementById("resultat").innerText = "Match Nul"
+                final = 0; //Match nul
+            }
+            else if (IA == 1) {
+                final = 1; //Victoire
+            }
+            else if (IA == 2) {
+                final = 2; //Défaite
+            }
         }
-        else if (IA == 1)
-        {
-            final = 1; //Victoire
+
+
+        //Si l'utilisateur joue FEUILLE
+        if (userChoice == 1) {
+            if (IA == 0) {
+                final = 2; //Défaite
+            }
+            else if (IA == 1) {
+                final = 0; //Match Nul
+            }
+            else if (IA == 2) {
+                final = 1; //Victoire
+            }
         }
-        else if (IA == 2)
-        {
-            final = 2; //Défaite
+
+        //Si l'utilisateur joue PIERRE
+        if (userChoice == 2) {
+            if (IA == 0) {
+                final = 1; //Victoire
+            }
+            else if (IA == 1) {
+                final = 2; //Défaite
+            }
+            else if (IA == 2) {
+                final == 0; //Match Nul
+            }
         }
-    }
 
-
-    //Si l'utilisateur joue FEUILLE
-    if (userChoice == 1)
-    {
-        if (IA == 0)
-        {
-            final = 2; //Défaite
+        if (IA == 0) {
+            document.getElementById("ordinateur").classList.add("result")
+            document.getElementById("ordinateur").innerText = "Ciseaux"
+            console.log("L'ordinateur a joué Ciseaux");
         }
-        else if (IA == 1)
-        {
-            final = 0; //Match Nul
+        else if (IA == 1) {
+            document.getElementById("ordinateur").classList.add("result")
+            document.getElementById("ordinateur").innerText = "Feuille"
+            console.log("L'ordinateur a joué Feuille");
         }
-        else if (IA == 2)
-        {
-            final = 1; //Victoire
+        else if (IA == 2) {
+            document.getElementById("ordinateur").classList.add("result")
+            document.getElementById("ordinateur").innerText = "Pierre"
+            console.log("L'ordinateur a joué Pierre");
         }
-    }
 
-    //Si l'utilisateur joue PIERRE
-    if (userChoice == 2)
-    {
-        if (IA == 0)
-        {
-            final = 1; //Victoire
+
+        if (final == 0) {
+            document.getElementById("resultat").classList.add("result")
+            document.getElementById("resultat").innerText = "Match Nul"
+            console.log("Match Nul");
         }
-        else if (IA == 1)
-        {
-            final = 2; //Défaite
+        else if (final == 1) {
+            document.getElementById("resultat").classList.add("result")
+            document.getElementById("resultat").innerText = "Vous avez gagné"
+            console.log("Gagné");
         }
-        else if (IA == 2)
-        {
-            final == 0; //Match Nul
+        else if (final == 2) {
+            document.getElementById("resultat").classList.remove("result")
+            document.getElementById("resultat").classList.add("result")
+            document.getElementById("resultat").innerText = "Vous avez perdu"
+            console.log("Perdu");
         }
-    }
 
-    if (IA == 0)
-    {
-        document.getElementById("ordinateur").classList.add("result")
-        document.getElementById("ordinateur").innerText="Ciseaux"
-        console.log("L'ordinateur a joué Ciseaux");
-    }
-    else if (IA == 1)
-    {
-        document.getElementById("ordinateur").classList.add("result")
-        document.getElementById("ordinateur").innerText="Feuille"
-        console.log("L'ordinateur a joué Feuille");
-    }
-    else if (IA == 2)
-    {
-        document.getElementById("ordinateur").classList.add("result")
-        document.getElementById("ordinateur").innerText="Pierre"
-        console.log("L'ordinateur a joué Pierre");
-    }
+        const NomJoueur = document.getElementById("PlayerName").value;
+        const TableHist = document.getElementById("HistoriqueSelec");
+        const ligne = document.createElement("tr");
+        const choix = document.getElementById("Joueur").textContent;
+        const fin = document.getElementById("ordinateur").textContent;
+
+        const cellulenom = document.createElement("td");
+        cellulenom.innerText = NomJoueur;
+        ligne.appendChild(cellulenom);
+
+        const cellulechoix = document.createElement("td");
+        cellulechoix.innerText = choix;
+        ligne.appendChild(cellulechoix);
+
+        const cellulefin = document.createElement("td");
+        cellulefin.innerText = fin;
+        ligne.appendChild(cellulefin);
+
+        const celluleres = document.createElement("td");
+        if (final == 0) {
+            celluleres.style.backgroundColor = "yellow";
+        }
+        else if (final == 1) {
+            celluleres.style.backgroundColor = "green";
+        }
+        else if (final == 2) {
+            celluleres.style.backgroundColor = "red";
+        }
+        ligne.appendChild(celluleres);
 
 
-    if (final == 0)
-    {
-        document.getElementById("resultat").classList.add("result")
-        document.getElementById("resultat").innerText="Match Nul"
-        console.log("Match Nul");
+        TableHist.appendChild(ligne);
     }
-    else if (final == 1)
-    {
-        document.getElementById("resultat").classList.add("result")
-        document.getElementById("resultat").innerText="Vous avez gagné"
-        console.log("Gagné");
-    }
-    else if (final == 2)
-    {
-        document.getElementById("resultat").classList.remove("result")
-        document.getElementById("resultat").classList.add("result")
-        document.getElementById("resultat").innerText="Vous avez perdu"
-        console.log("Perdu");
-    }
-
-    const NomJoueur = document.getElementById("PlayerName").value
-    const TableHist = document.getElementById("HistoriqueSelec");
-    const ligne = document.createElement("tr");
-    const choix = document.getElementById("Joueur").textContent;
-    const fin = document.getElementById("ordinateur").textContent;
-
-    const cellulenom = document.createElement("td");
-    cellulenom.innerText = NomJoueur;
-    ligne.appendChild(cellulenom);
-
-    const cellulechoix = document.createElement("td");
-    cellulechoix.innerText = choix;
-    ligne.appendChild(cellulechoix);
-
-    const cellulefin = document.createElement("td");
-    cellulefin.innerText = fin;
-    ligne.appendChild(cellulefin);
-
-    const celluleres = document.createElement("td");
-    if (final == 0)
-    {
-        celluleres.style.backgroundColor="yellow";
-    }
-    else if (final == 1)
-    {
-        celluleres.style.backgroundColor="green";
-    }
-    else if (final == 2)
-    {
-        celluleres.style.backgroundColor="red";
-    }
-    ligne.appendChild(celluleres);
-
-    
-    TableHist.appendChild(ligne);
-
 }
